78a40eaf10a62b305ea5f77d4d9041eaedffa7fe
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-01-06 13:45:41 -0800

    Styled the mobile navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,19 @@
-import styles from './navbar.modules.css';
+import styles from './navbar.module.css';
+
+import menu from '../../assets/images/icon-menu.svg';
+import logo from '../../assets/images/logo.svg';
 
 const Navbar = () => {
-  return <header className={styles.navebarContainer}></header>;
+  return (
+    <header className={styles.navbarContainer}>
+      <div className={styles.navbarLogo}>
+        <img src={logo} alt='News logo' />
+      </div>
+      <div className={styles.navbarMenu}>
+        <img src={menu} alt='Hamburger menu icon' />
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
